6988876997b3815e824ebfcd15c2329cbaa06305
Romel Rey Silva <sromelrey@gmail.com>
2024-05-17 00:02:32 +0800

    working project animation

--- app/(sections)/about.tsx
+++ app/(sections)/about.tsx
@@ -8,11 +8,15 @@ export default function About() {
       id='about'
       className={` min-w-full h-lvh justify-center flex flex-col gap-5 p-6 max-w-[950px]  ${open.className}`}
     >
-      <h1 className={`text-white text-6xl font-bold font-sans`}>
+      <h1
+        className={`animate-slidein opacity-0 [--slidein-delay:300ms] text-white text-6xl font-bold font-sans`}
+      >
         Hi, I’m Romel Rey Silva
       </h1>
-      <h3 className='text-[#00B2EB] font-bold text-5xl'>Frontend Developer</h3>
-      <h5 className='text-white text-xl font-bold'>
+      <h3 className='animate-slidein opacity-0 [--slidein-delay:500ms] text-[#00B2EB] font-bold text-5xl'>
+        Frontend Developer
+      </h3>
+      <h5 className='animate-slidein opacity-0 [--slidein-delay:700ms] text-white text-xl font-bold'>
         Proficient in REACTJS for more than 5 years, refining abilities across
         Front-End, Full-Stack, and Software Engineering domains. Embracing
         adaptability as an ongoing journey.
--- app/(sections)/projects.tsx
+++ app/(sections)/projects.tsx
@@ -2,35 +2,42 @@ import React from "react";
 import { Open_Sans } from "next/font/google";
 import Image from "next/image";
 const open = Open_Sans({ subsets: ["hebrew"] });
-
+import styles from "./project.module.css";
 export default function Projects() {
   return (
     <section
       id='projects'
       className={`min-w-full h-lvh justify-center flex flex-col gap-8 p-6 max-w-[950px]  ${open.className}`}
     >
-      <h1 className={`text-6xl font-bold font-sans text-center`}>
+      <h1
+        className={`animate-slidein opacity-0 [--slidein-delay:300ms] text-6xl font-bold font-sans text-center`}
+      >
         The Projects is divided into two sections:
         <span className='text-[#D05151] '> professional work </span>and
         <span className='text-[#00B2EB] '>personal passion</span> , each
         showcasing distinct facets of my skills and interests.
       </h1>
-      <h1 className={`  text-7xl font-bold font-sans text-center`}>
+      <h1
+        className={`animate-slidein opacity-0 [--slidein-delay:500ms]  text-7xl font-bold font-sans text-center`}
+      >
         <span className='text-[#D05151] '> Choose </span> your
         <span className='text-[#00B2EB] '> pill</span>
       </h1>
-      <div className='h-2/4 flex flex-row gap-64 justify-center'>
-        <div>
+      <div className='animate-slidein opacity-0 [--slidein-delay:700ms] h-2/4 flex flex-row gap-64 justify-center'>
+        <div className={styles.image}>
           <Image
             src='/red_pill.png'
             width={340}
             height={300}
-            className='hidden md:block'
+            className={`hidden md:block `}
             alt='Screenshots of the dashboard project showing desktop and mobile versions'
           />
+          <h1 className='text-[#D05151] text-4xl text-center font-bold'>
+            Professional Work
+          </h1>
         </div>
 
-        <div>
+        <div className={styles.image}>
           <Image
             src='/blue_pill.png'
             width={340}
@@ -38,6 +45,9 @@ export default function Projects() {
             className='hidden md:block'
             alt='Screenshots of the dashboard project showing desktop and mobile versions'
           />
+          <h1 className='text-[#00B2EB]  text-4xl text-center font-bold'>
+            Personal Passion
+          </h1>
         </div>
       </div>
     </section>
